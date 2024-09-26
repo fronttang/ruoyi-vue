1c743b7a0e7e02626fd6240bfcd499b221c953c3
FrontTang <fronttang@gmail.com>
2024-09-26 09:47:40 +0800

    光伏储能电力设计后台功能

---
+++ ruoyi-ui/src/views/electrical/account/index.vue
@@ -0,0 +1,351 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="姓名/账号" prop="account" label-width="100px">
+        <el-input
+          v-model="queryParams.account"
+          placeholder="请输入姓名/账号"
+          clearable
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item label="账号类型" prop="accountType">
+          <el-select v-model="queryParams.accountType" placeholder="请选择账号类型" clearable filterable>
+            <el-option
+              v-for="dict in dict.type.storage_account_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          plain
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+        >删除</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="detectUnitUserList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="ID" align="center" width="60" prop="id" />
+      <el-table-column label="姓名" align="center" min-width="120" prop="name" />
+      <el-table-column label="账号" align="center" width="120" prop="account" />
+      <el-table-column label="账号类型" align="center" width="120" prop="accountType" >
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.storage_account_type" :value="scope.row.accountType"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="状态" align="center" width="60" prop="status">
+      <template slot-scope="scope">
+        <el-switch
+          v-model="scope.row.status"
+          active-value="0"
+          inactive-value="1"
+          @change="handleStatusChange(scope.row)"
+        ></el-switch>
+      </template>
+      </el-table-column>
+      <el-table-column label="最后修改时间" align="center" prop="updateTime" width="160">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.updateTime) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" align="center" width="180" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleUpdate(scope.row)"
+          >修改</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+          >删除</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleLogout(scope.row)"
+          >下线</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改电力设计账号对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="账号类型" prop="accountType">
+          <el-select v-model="form.accountType" placeholder="请选择账号类型">
+            <el-option
+              v-for="dict in dict.type.storage_account_type"
+              :key="dict.value"
+              :label="dict.label"
+              :value="dict.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="姓名" prop="name">
+          <el-input v-model="form.name" placeholder="请输入姓名" />
+        </el-form-item>
+        <el-form-item label="账号" prop="account">
+          <el-input v-model="form.account" placeholder="请输入账号" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password" placeholder="请输入密码" type="password" maxlength="20" show-password/>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="confirmPassword" v-if="this.form.password !== '********'">
+          <el-input v-model="form.confirmPassword" placeholder="请确认密码" type="password" maxlength="20" show-password/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listDetectUnitUser, getDetectUnitUser, delDetectUnitUser, addDetectUnitUser, updateDetectUnitUser, userLogout } from "@/api/projectrole/detectUnitUser";
+import { detectUnitDict } from "@/api/projectrole/DetectUnit";
+import { changeUserStatus } from "@/api/system/user";
+
+export default {
+  name: "DetectUnitUser",
+  dicts: ['account_status', 'storage_account_type'],
+  data() {
+    const equalToPassword = (rule, value, callback) => {
+      if (this.form.password !== value) {
+        callback(new Error("两次输入的密码不一致"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 检测单位账号表格数据
+      detectUnitUserList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        account: null,
+        type: '06',
+        accountType: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+        accountType: [
+          { required: true, message: "请选择账号类型", trigger: "change" }
+        ],
+        name: [
+          { required: true, message: "姓名不能为空", trigger: "blur" }
+        ],
+        account: [
+          { required: true, message: "账号不能为空", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 20, message: '长度在 6 到 20 个字符', trigger: 'blur' },
+          { pattern: /^[^<>"'|\\]+$/, message: "不能包含非法字符：< > \" ' \\\ |", trigger: "blur" }
+        ],
+        confirmPassword: [
+          { required: true, message: '请确认密码', trigger: 'blur' },
+          { required: true, validator: equalToPassword, trigger: 'blur' }
+        ],
+      },
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询检测单位账号列表 */
+    getList() {
+      this.loading = true;
+      listDetectUnitUser(this.queryParams).then(response => {
+        this.detectUnitUserList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        detectId: null,
+        detectName: null,
+        account: null,
+        name: null,
+        password: null,
+        confirmPassword: null,
+        status: null,
+        type: '06',
+        accountType: null,
+        createBy: null,
+        createTime: null,
+        updateBy: null,
+        updateTime: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加电力设计账号";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      detectUnitDict().then(response => {
+        this.detectUnitDict = response.data;
+      });
+      getDetectUnitUser(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改电力设计账号";
+      });
+    },
+    // 用户状态修改
+    handleStatusChange(row) {
+      let text = row.status === "0" ? "启用" : "停用";
+      this.$modal.confirm('确认要"' + text + '""' + row.account + '"用户吗？').then(function() {
+        return changeUserStatus(row.id, row.status);
+      }).then(() => {
+        this.$modal.msgSuccess(text + "成功");
+      }).catch(function() {
+        row.status = row.status === "0" ? "1" : "0";
+      });
+    },
+    handleLogout(row){
+      const ids = row.id || this.ids;
+      this.$modal.confirm('确认要强制下线编号为' + ids + '的用户吗？').then(function() {
+        return userLogout(ids);
+      }).then(() => {
+        this.$modal.msgSuccess("下线成功");
+      }).catch(function() {
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateDetectUnitUser(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addDetectUnitUser(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除电力设计账号编号为"' + ids + '"的数据项？').then(function() {
+        return delDetectUnitUser(ids);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('projectrole/detectUnitUser/export', {
+        ...this.queryParams
+      }, `detectUnitUser_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
